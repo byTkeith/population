00c70f943387684a7a88b0e362a881bef31fcafd
Your Name <you@example.com>
2023-12-20 21:01:19 +0200

    used scanner to open the file

---
+++ Population.java
@@ -0,0 +1,11 @@
+//program that creates critter strings
+//Tendai K  Nyevedzanai
+import java.util.*;
+import java.io.*;
+public class Population{
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        
+    }
+
+}
